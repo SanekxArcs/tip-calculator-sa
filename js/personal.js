7689a260012e7a20b5320f7657f27c8aa08513be
Oleksandr Dzisiak <shumahersasha007@gmail.com>
2022-12-06 03:19:44 +0100

    minor changes

--- js/personal.js
+++ js/personal.js
@@ -1,25 +1,23 @@
-'use strict';
+"use strict";
 
-const personInput = document.querySelector('.people'),
-      personsWindow = document.querySelector('.container-for-persons'),
-      resultText = document.querySelector('.js-list-of-person-results'),
-      nameInputsText = document.querySelectorAll('.person-name'),
-      billInputsValue = document.querySelectorAll('.person-bill'),
-      IdNumber = document.querySelectorAll('.number-text');
+const personInput = document.querySelector(".people"),
+  personsWindow = document.querySelector(".container-for-persons"),
+  resultText = document.querySelector(".js-list-of-person-results"),
+  nameInputsText = document.querySelectorAll(".person-name"),
+  billInputsValue = document.querySelectorAll(".person-bill"),
+  IdNumber = document.querySelectorAll(".number-text");
 
+personInput.addEventListener("input", createInputs);
 
-
-personInput.addEventListener('input', createInputs);
-
-function createInputs(){
-  let clearDivs = document.querySelectorAll('.container-for-persons');
-  let clearResurtDivs = document.querySelectorAll('.list-of-person-results');
+function createInputs() {
+  let clearDivs = document.querySelectorAll(".container-for-persons");
+  let clearResurtDivs = document.querySelectorAll(".list-of-person-results");
   clearDivs.forEach((e) => {
     e.innerHTML = "";
   });
   clearResurtDivs.forEach((e) => {
     e.innerHTML = "";
-  })
+  });
   let valuePersons = personInput.value;
   console.log(valuePersons);
   for (let index = 1; index <= valuePersons; index++) {
@@ -38,7 +36,7 @@ function createInputs(){
       <input type="number" class="person-bill input">
     </div>
   </div>`;
-  resultText.innerHTML += `<div class="list-of-person-results">
+    resultText.innerHTML += `<div class="list-of-person-results">
 
   <div class=" check-boxes check-boxes-result">
     <input id="${index}" type="checkbox" class="material_checkbox">
@@ -55,22 +53,31 @@ function createInputs(){
   <div class="personal-bill-result">
     <p class="person-bill-result">49.00</p>
   </div>
-</div>`
+</div>`;
   }
-  return
-};
+  return;
+}
 
-personsWindow.addEventListener('iuput', forEach(recordData )
-);
+//create function for record many inputs data and add to array of objects?
+let artists = [];
 
-let dataBill = [0];
+function addArtist(artistsToAdd) {
+  artists.push(...artistsToAdd);
+  return artists;
+}
 
-function recordData(e) {
-  dataBill += {
-    id: IdNumber,
-    name: nameInputsText,
-    bill: billInputsValue
-  }
-};
-
-console.log(dataBill);
+const objects = [
+  {
+    rank: 3,
+    name: "Bob",
+    year: 1995,
+    album: "Album3",
+  },
+  {
+    rank: 4,
+    name: "Nas",
+    year: 1992,
+    album: "Album4",
+  },
+];
+console.log(addArtist(objects));
