1bfe8fa84d8209078c8c61e472794a1718d0c8d3
Oleksandr Dzisiak <shumahersasha007@gmail.com>
2022-12-07 22:39:12 +0100

    change slider

--- js/windowChange.js
+++ js/windowChange.js
@@ -4,18 +4,17 @@ const slideValue = document.querySelector(".sliderValue span");
 const inputSlider = document.querySelector(".field input");
 
 slideValue.style.display = 'none';
-
 inputSlider.oninput = (()=>{
   let value = inputSlider.value;
   slideValue.style.display = '';
-  if (value < 5){
+  if (value < 10){
     slideValue.textContent = value;
     slideValue.style.width = (10) + "%";
     slideValue.style.left = (10) + "%";
   }else {
     slideValue.textContent = value;
-  slideValue.style.width = (value/2) + "%";
-  slideValue.style.left = `calc(${(value*2) + "%"})`;
+  slideValue.style.width = (value) + "%";
+  slideValue.style.left = `calc(${(value) + "%"})`;
   }
   slideValue.classList.add("show");
 });
